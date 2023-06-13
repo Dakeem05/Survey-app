13fe008de50e388aab9519e4aba5761c572769b9
Dakeem05 <edidiongsamuel14@gmail.com>
2023-06-13 17:00:10 +0100

    Progres

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -213,6 +213,7 @@ const store = createStore({
             token:sessionStorage.getItem('TOKEN')
         },
         surveys:[...tmpSurveys],
+        questionTypes:['text', 'select', 'radio', 'checkbox', 'textarea'],
     },
     getter:{},
     actions:{
